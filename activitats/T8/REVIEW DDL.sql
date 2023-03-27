58f0869e2d52c2af503f18d5e5a7dbbab46f1c8a
Marc <marcsans26@gmail.com>
2023-03-27 20:38:46 +0200

    ns

---
+++ activitats/T8/REVIEW DDL.sql
@@ -0,0 +1,9 @@
+-- 1. Change PK of COLLECCIONS for an integer auto_increment.
+
+
+-- 2. Change PK of DEPARTMENTS for an integer auto_increment
+
+
+-- 3. Change PK of LLENGUES for an integer auto_increment
+
+

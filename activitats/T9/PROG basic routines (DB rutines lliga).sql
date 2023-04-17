a790faad6fbfe02849660821d41df3202d92fb79
Marc <marcsans26@gmail.com>
2023-04-17 17:01:10 +0200

    acabar activitats

--- activitats/T9/PROG basic routines (DB rutines lliga).sql
+++ activitats/T9/PROG basic routines (DB rutines lliga).sql
@@ -1,4 +1,4 @@
--- DELIMITER &&
+DELIMITER &&
 
 USE rutines;
 
@@ -20,7 +20,7 @@ BEGIN
 	set resultat = upper(SUBSTRING('marc', 1, 3));
 
 	SELECT resultat;
-END;
+END &&
 
 
 CALL primeres3('hola radiola');
@@ -52,30 +52,83 @@ BEGIN
         SET max_num = tercer;
     END IF;
     RETURN max_num;
-END;
+END &&
 
 
 SELECT major(5,34,12); -- 34
 SELECT major(34,5,12);
 SELECT major(12,5,34);
 -- --------------------------------------------------------------------------------------------------------------------------
-USE liga;
+USE lliga;
 
 -- 5. On the database "lliga", create a function that calculates the number of points taking as parameter the match result in format 'xxx-xxx'. Test your function with the field <resultat> of table "partits".
 -- HINT: use the "substring_index" function.
-select '123-98',substring_index('123-98','-',1),substring_index('123-98','-',-1), 
-substring_index('123-98','-',1)+substring_index('123-98','-',-1;
+select '123-98',
+    substring_index('123-98','-',1),
+    substring_index('123-98','-',-1), 
+    substring_index('123-98','-',1) + substring_index('123-98','-',-1) &&
 
+CREATE FUNCTION total_punts(resultat VARCHAR(7)) RETURNS INT
+reads sql data
+BEGIN
+    DECLARE punts_local INT;
+    DECLARE punts_visitant INT;
+    SET punts_local = SUBSTRING_INDEX(resultat, '-', 1);
+    SET punts_visitant = SUBSTRING_INDEX(resultat, '-', -1);
+    RETURN punts_local + punts_visitant;
+END &&
 
-
-select total_punts('78-100'); -- 178
-select elocal,evisitant,resultat,total_punts(resultat) PUNTS from partit; -- Check your function with this query
+select total_punts('78-100') && -- 178
+select elocal,evisitant,resultat,total_punts(resultat) as PUNTS
+from partits &&
 
 -- 6. On the database "lliga", create a function that returns true (1) if the visitor wins and false (0) otherwise. The input parameter is the match result in format 'xxx-xxx'.
-
+create function guanyador (resultat varchar(8)) returns tinyint
+reads sql data
+begin
+	declare x int;
+    declare y int;
+    declare z tinyint;
+    set x = substring_index(resultat,'-',1);
+    set y = substring_index(resultat,'-',-1);
+    
+    if x > y then
+		set z = 0;
+	elseif y > x then
+		set z = 1;
+	end if;
+	return z;
+end &&
 
 
 
 SELECT guanyador('88-79'); -- 0
 SELECT guanyador('79-88'); -- 1
 SELECT elocal,evisitant,resultat,guanyador(resultat) FROM partits; -- Check your function with this query
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

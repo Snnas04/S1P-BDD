57d4dc3fb4c144b7d4cd57ceacf296f4025874b1
Marc <marcsans26@gmail.com>
2023-05-15 20:54:45 +0200

    usuaris

--- activitats/T10/usuaris.sql
+++ activitats/T10/usuaris.sql
@@ -1,5 +1,5 @@
 SELECT * FROM mysql.user;
 
-create user test@localhost identified by 'password'; -- indicam el nom d'usuari, l'equip del qual se pot connectar i una contrasenya, si no especificam el servidor per defecta posa % "qualsevol"
+create user test@localhost identified by 'test'; -- indicam el nom d'usuari, l'equip del qual se pot connectar i una contrasenya, si no especificam el servidor per defecta posa % "qualsevol"
 
 drop user test;

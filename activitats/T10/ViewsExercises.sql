734ca66d120e2ff194fd384c0d0db408fc810af9
Marc <marcsans26@gmail.com>
2023-05-19 16:20:00 +0200

    activitat

--- activitats/T10/ViewsExercises.sql
+++ activitats/T10/ViewsExercises.sql
@@ -13,6 +13,31 @@ create view CompteComandesMadrid as select NomClient,
 -- 2
 -- No he posat que l’usuari es connecti només des del localhost perquè al emprar docker me dona error a l’hora d’intentar connectar-me
 create user X identified by 'X';
-grant select (NomClient, NomContacte) on CompteComandesMadrid to X;
-grant select on Clients.* to X;
+grant select (NomClient, NomContacte, NumeroDeComandes) on CompteComandesMadrid to X;
+grant select on Clients to X;
 
+show grants for X;
+
+-- 3
+-- s'ha d'executrar desde l'usuari X, per complir amb els requisits de l'exercisi
+select cm.`NomClient`, c.`CodiPostal`, c.`Telefon`
+from `CompteComandesMadrid` cm
+join `Clients` as c on c.`NomClient` = cm.`NomClient`
+order by `NumeroDeComandes` desc, cm.NomClient asc
+limit 5;
+
+-- 4
+create view Top5Madrid as select cm.`NomClient`, c.`CodiPostal`, c.`Telefon`
+                            from `CompteComandesMadrid` cm
+                            join `Clients` as c on c.`NomClient` = cm.`NomClient`
+                            order by `NumeroDeComandes` desc, cm.NomClient asc
+                            limit 5;
+
+grant select (NomClient, CodiPostal, Telefon) on Top5Madrid to X;
+
+-- 5
+revoke select (NomClient, NomContacte, NumeroDeComandes) on CompteComandesMadrid from X;
+revoke select on Clients from X;
+
+select *
+from Top5Madrid;
